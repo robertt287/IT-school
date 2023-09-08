57dc9070469bbebfefdffb2f69a6916f244ccaed
robertt287 <dragomirr94@gmail.com>
2023-09-08 19:49:06 +0300

    Created session 2 package

--- src/session2/test.java
+++ src/session2/test.java
@@ -3,6 +3,6 @@ package session2;
 public class test {
 public static void main(String[] args){
     System.out.println("hello");
-    //this is a comment from main branch.
+    //this is a comment from main branch..
 }
 }
