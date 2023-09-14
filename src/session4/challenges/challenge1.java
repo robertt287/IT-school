5d9930c6660f4aec9dbec7a945631dee608f2e94
robertt287 <dragomirr94@gmail.com>
2023-09-15 01:26:20 +0300

    session 4   9.Challenges

---
+++ src/session4/challenges/challenge1.java
@@ -0,0 +1,24 @@
+package session4.challenges;
+
+//1. Write a Java program where you create two String objects named stringOne and stringTwo with the same value "OpenAI".
+// Check and print whether they refer to the same object.
+
+public class challenge1 {
+
+    public static void main(String[] args) {
+        String stringOne = "OpenAI";
+        String stringTwo = "OpenAI";
+
+        //check if strings are the same
+        boolean areSame = stringOne==stringTwo;
+
+        if (areSame) {
+            System.out.println("they refer to the same object.");
+        } else {
+            System.out.println("they do not refer to the same object.");
+        }
+
+    }
+
+
+}
---
+++ src/session4/challenges/challenge8.java
@@ -0,0 +1,17 @@
+package session4.challenges;
+
+//8. Write a Java program to calculate the factorial of a number using a for loop. Declare an integer variable number and assign a value to it.
+// Then calculate and print the factorial of number.
+public class challenge8 {
+    public static void main(String[] args) {
+        int fact = 1;
+        int number = 5;
+
+        for (int i = 1; i <= number; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("The factorial number for " + number + " is " + fact);
+    }
+}
+
